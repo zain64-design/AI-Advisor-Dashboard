32232ff8c1af20a2bbf5cbab6bbbc2b6904e9ad1
zain64-design <zainyzain64@example.com>
2024-12-26 06:44:22 +0500

    login flow completed

--- src/components/Auth/OTPModal.jsx
+++ src/components/Auth/OTPModal.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Modal from 'react-bootstrap/Modal';
 import { Button,Form } from 'react-bootstrap';
 import Text from '../UI/Text'
 import Image from '../UI/Image'
 import messageIcon from '/images/icons/message-icon.svg'
 import '../../assets/scss/component/Modal/otpModal.scss'
+import { InputOtp } from 'primereact/inputotp';
 
 const OTPModal = ({showOTPModal,handleCloseOTPModal}) => {
+
+        const [token, setToken] = useState('');
+
+        const customInput = ({ events, props }) => (
+            <input {...events} {...props} type="text" className="custom-otp-input" />
+        );
+    
     return (
         <>
             <Modal size="lg" className='otp-modal' show={showOTPModal} onHide={handleCloseOTPModal} aria-labelledby="contained-modal-title-vcenter" centered>
@@ -20,7 +28,18 @@ const OTPModal = ({showOTPModal,handleCloseOTPModal}) => {
                         <Text as='p'>example@site.com</Text>
 
                         <div className="otp-number-area">
+                            <Form>
+                            <InputOtp
+                                    value={token}
+                                    onChange={(e) => setToken(e.value)}
+                                    inputTemplate={customInput}
+                                    integerOnly
+                                    numInputs={4} // Set the number of OTP inputs
+                                />
+                            </Form>
                         </div>
+
+                        <Text as='small'>Didn’t receive code? <Button type="button" variant='resend'>Resend code</Button></Text>
                     </div>
                     <Modal.Footer>
                         <Button variant="okay" onClick={handleCloseOTPModal}>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import './App.scss'
 
 createRoot(document.getElementById('root')).render(
